eb961eb28821bd3f2f60de9ce8ac2998afecdc33
Subin An <subinium@gmail.com>
2021-10-07 00:17:08 +0900

    close #7 : add cvae model and easy config setting

--- dataset.py
+++ dataset.py
@@ -12,6 +12,6 @@ def mnist_dataloader(dataset_path, batch_size):
     train_dataset = MNIST(dataset_path, transform=transform, train=True, download=True)
     test_dataset  = MNIST(dataset_path, transform=transform, train=False, download=True)
 
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
     test_loader  = DataLoader(dataset=test_dataset,  batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
     return train_loader, test_loader
--- models/CVAE.py
+++ models/CVAE.py
@@ -6,10 +6,11 @@ class Encoder(nn.Module):
     def __init__(self, args):
         super().__init__()
         input_dim = args['input_dim']
+        condition_dim = args['condition_dim']
         hidden_dim = args['hidden_dim']
         latent_dim = args['latent_dim']
         self.model = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
+            nn.Linear(input_dim+condition_dim, hidden_dim),
             nn.LeakyReLU(0.2),
             nn.Linear(hidden_dim, hidden_dim),
             nn.LeakyReLU(0.2)
@@ -17,8 +18,8 @@ class Encoder(nn.Module):
         self.fc_mean = nn.Linear(hidden_dim, latent_dim)
         self.fc_var = nn.Linear(hidden_dim, latent_dim)
     
-    def forward(self, x):
-        h = self.model(x)
+    def forward(self, x, c):
+        h = self.model(torch.cat([x, c], 1))
         mu = self.fc_mean(h)
         logvar = self.fc_var(h)
         return mu, logvar
@@ -27,11 +28,12 @@ class Decoder(nn.Module):
     def __init__(self, args):
         super().__init__()
         input_dim = args['input_dim']
+        condition_dim = args['condition_dim']
         hidden_dim = args['hidden_dim']
         latent_dim = args['latent_dim']
         output_dim = args['output_dim']
         self.model = nn.Sequential(
-            nn.Linear(latent_dim, hidden_dim),
+            nn.Linear(latent_dim+condition_dim, hidden_dim),
             nn.LeakyReLU(0.2),
             nn.Linear(hidden_dim, hidden_dim),
             nn.LeakyReLU(0.2),
@@ -39,10 +41,10 @@ class Decoder(nn.Module):
             nn.Sigmoid()
         )
     
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, z, c):
+        return self.model(torch.cat([z, c], 1))
 
-class VanillaVAE(nn.Module):
+class CVAE(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.encoder = Encoder(args)
@@ -50,20 +52,22 @@ class VanillaVAE(nn.Module):
         
     def reparametrize(self, mu, logvar):
         epsilon = torch.randn_like(logvar)
-        z = mu + logvar*epsilon
+        z = mu + torch.exp(0.5*logvar)*epsilon
         return z
 
     def loss_calc(self, x, x_hat, mu, logvar):
-        reproduction_loss = F.binary_cross_entropy(x_hat, x, reduction='sum')
+        BCE = F.binary_cross_entropy(x_hat, x, reduction='sum')
         KLD = - 0.5 * torch.sum(1+ logvar - mu.pow(2) - logvar.exp())
-        return reproduction_loss + KLD
+        return BCE + KLD
 
-    def forward(self, x):
-        mu, logvar = self.encoder(x)
+    def forward(self, x, c):
+        mu, logvar = self.encoder(x, c)
         z = self.reparametrize(mu, logvar)
-        x_hat = self.decoder(z)
+        x_hat = self.decoder(z, c)
         loss = self.loss_calc(x, x_hat, mu, logvar)
         return x_hat, loss
+
+
 
 if __name__ == '__main__':
     args = {"input_dim":784, "hidden_dim":100, "latent_dim":50, "output_dim":784}
