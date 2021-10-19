1e8919e2bd7eee99e85f095676efe1369c0df063
Subin An <subinium@gmail.com>
2021-10-19 17:19:23 +0900

    update #10 : add celebA dataset / update main.py for BetaVAE

--- dataset.py
+++ dataset.py
@@ -1,7 +1,17 @@
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, ImageFolder
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
+class CustomImageFolder(ImageFolder):
+    def __init__(self, root, transform=None):
+        super(CustomImageFolder, self).__init__(root, transform)
+
+    def __getitem__(self, index):
+        path = self.imgs[index][0]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img
 
 def mnist_dataloader(dataset_path, batch_size):
     transform = transforms.Compose([
@@ -15,3 +25,26 @@ def mnist_dataloader(dataset_path, batch_size):
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
     test_loader  = DataLoader(dataset=test_dataset,  batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
     return train_loader, test_loader
+
+def celebA_dataloader(dataset_path, batch_size): 
+    image_size = 64
+    transform = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            
+    ])
+    train_data = CustomImageFolder(root='./dataset/celeba/', transform=transform)
+    train_loader = DataLoader(train_data,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=1,
+                              pin_memory=True,
+                              drop_last=True)
+
+
+    
+#     test_loader  = DataLoader(dataset=test_dataset,  batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
+    return train_loader#, test_loader
+
+if __name__ == '__main__':
+    celebA_dataloader('./', 32)
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from torchvision.utils import save_image, make_grid
 # custom modules
 from models.VanillaVAE import VanillaVAE
 from models.CVAE import CVAE
+from models.BetaVAE  import BetaVAE
 from dataset import *
 from logger import *
 
@@ -22,19 +23,25 @@ def train(args, device):
         model = VanillaVAE(args).to(device)
     elif model_name == 'CVAE':
         model = CVAE(args).to(device)
+    elif model_name == 'BetaVAE':
+        model = BetaVAE(args).to(device)
     optimizer = Adam(model.parameters(), lr=args['lr'])
     logger = prepare_logger()
     
-    train_loader, valid_loader = mnist_dataloader('./dataset', args['batch_size'])
+    if model_name != 'BetaVAE':
+        train_loader, valid_loader = mnist_dataloader('./dataset', args['batch_size'])
+    else:
+        train_loader = celebA_dataloader('./', 32)
     print('Number of Parameters: ', sum(param.numel() for param in model.parameters()))
     
     
     model.train()
     iteration = 0
     for epoch in range(args['epochs']):
-        for idx, (x, label) in enumerate(train_loader):
-            iteration += 1
-            
+        for idx, x in enumerate(train_loader):
+            iteration += 1            
+            if model_name == 'CVAE':
+                x, label = x
             x = x.to(device)
 
             if model_name == 'VAE':
@@ -42,12 +49,14 @@ def train(args, device):
             if model_name == 'CVAE':
                 label_ohe = F.one_hot(label, num_classes=10).to(device)
                 x_hat, loss = model(x, label_ohe)
+            if model_name == 'BetaVAE':
+                x_hat, loss = model(x)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             if iteration % 100 == 0:
                 logger.add_scalar('Loss/train', loss, iteration)
-                logger.add_images('Image/train', torch.stack([x.cpu(), x_hat.cpu()], dim=0).view(-1, 1, 28, 28), iteration)
+                logger.add_images('Image/train', torch.stack([x.cpu(), x_hat.cpu()], dim=0).view(-1, *(x.shape[1:])), iteration)
             
                 
 
