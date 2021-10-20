6db237a46cf323f7a4431c5a5425c73677641adb
Subin An <subinium@gmail.com>
2021-10-20 09:36:36 +0900

    update training process & README.md

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
   <a href="https://www.python.org/">
     <img src="https://img.shields.io/badge/Python-3.8-4B8BBE.svg" /></a>
   <a href= "https://pytorch.org/">
-    <img src="https://img.shields.io/badge/PyTorch-1.7-EE4C2C.svg" /></a>
+    <img src="https://img.shields.io/badge/PyTorch-1.9-EE4C2C.svg" /></a>
   <a href= "https://github.com/subinium/Pytorch-AutoEncoders/blob/main/LICENSE">
     <img src="https://img.shields.io/badge/license-MIT-yellow.svg" /></a>
 </p>
@@ -21,8 +21,8 @@ Implementing a Variational Autoencoder (VAE) Series in Pytorch.
 |-|-|-|-|
 |O|**VAE**|[Auto-Encoding Variational Bayes](https://arxiv.org/abs/1312.6114)|ICLR 2014|
 |O|**CVAE**|[Learning Structured Output Representation using Deep Conditional Generative Models](https://scholar.google.com/scholar_url?url=http://papers.nips.cc/paper/5775-learning-structured-outputrepresentation-using-deep-conditional-generative-models.pdf&hl=ko&sa=T&oi=gsb-gga&ct=res&cd=0&d=12314198516266869942&ei=dUBhYbqGLKXGywS7_I6ABQ&scisig=AAGBfm09qbvL0Wl0zrhnRZMZS7H26bQv4Q)|NeurIPS 2015|
-||**AAE**|[Adversarial Autoencoder](https://arxiv.org/abs/1511.05644)|ICLR 2016|
-||**Beta-VAE**|[β-VAE: Learning Basic Visual Concepts with a Constrained Variational Framework](https://openreview.net/forum?id=Sy2fzU9gl)|ICLR 2017|
+|O|**AAE**|[Adversarial Autoencoder](https://arxiv.org/abs/1511.05644)|ICLR 2016|
+|O|**Beta-VAE**|[β-VAE: Learning Basic Visual Concepts with a Constrained Variational Framework](https://openreview.net/forum?id=Sy2fzU9gl)|ICLR 2017|
 ||**VQ-VAE**|[Neural Discrete Representation Learning](https://arxiv.org/abs/1711.00937)|NeurIPS 2017|
 
 ## Result
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from torchvision.utils import save_image, make_grid
 from models.VanillaVAE import VanillaVAE
 from models.CVAE import CVAE
 from models.BetaVAE  import BetaVAE
+from models.AAE import AAE, Discriminator
 from dataset import *
 from logger import *
 
@@ -25,38 +26,65 @@ def train(args, device):
         model = CVAE(args).to(device)
     elif model_name == 'BetaVAE':
         model = BetaVAE(args).to(device)
+    elif model_name == 'AAE':
+        model = AAE(args).to(device)
+        model_D = Discriminator(args).to(device)
+
     optimizer = Adam(model.parameters(), lr=args['lr'])
+    if model_name == 'AAE':
+        optimizer_D = Adam(model_D.parameters(), lr=args['lr'])
+    
     logger = prepare_logger()
     
     if model_name != 'BetaVAE':
         train_loader, valid_loader = mnist_dataloader('./dataset', args['batch_size'])
     else:
         train_loader = celebA_dataloader('./', 32)
+    
     print('Number of Parameters: ', sum(param.numel() for param in model.parameters()))
     
     
+
     model.train()
     iteration = 0
     for epoch in range(args['epochs']):
         for idx, x in enumerate(train_loader):
             iteration += 1            
-            if model_name == 'CVAE':
+            if model_name in ['VAE', 'CVAE', 'AAE']:
                 x, label = x
             x = x.to(device)
 
-            if model_name == 'VAE':
+            
+
+            if model_name in ['VAE', 'BetaVAE']:
                 x_hat, loss = model(x)
             if model_name == 'CVAE':
                 label_ohe = F.one_hot(label, num_classes=10).to(device)
                 x_hat, loss = model(x, label_ohe)
-            if model_name == 'BetaVAE':
-                x_hat, loss = model(x)
+            if model_name == 'AAE':
+                valid = torch.ones((x.shape[0], 1), requires_grad=False).to(device)
+                fake =  torch.zeros((x.shape[0], 1), requires_grad=False).to(device)
+                x_hat, fake_z = model(x)
+                
+                loss = 0.001*F.binary_cross_entropy(model_D(fake_z), valid) + 0.999*F.mse_loss(x_hat.cuda(), x.cuda())
+                
+            
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+            if model_name == 'AAE':
+                optimizer_D.zero_grad()
+                real_z = torch.normal(0, 1, size=(x.shape[0], args['latent_dim'])).to(device)
+                real_loss = F.binary_cross_entropy(model_D(real_z), valid)
+                fake_loss = F.binary_cross_entropy(model_D(fake_z.detach()), fake)
+                D_loss = 0.5 * (real_loss + fake_loss)
+                D_loss.backward()
+                optimizer_D.step()
+
             if iteration % 100 == 0:
-                logger.add_scalar('Loss/train', loss, iteration)
-                logger.add_images('Image/train', torch.stack([x.cpu(), x_hat.cpu()], dim=0).view(-1, *(x.shape[1:])), iteration)
+                logger.add_scalar(f'Loss/train_{model_name}', loss, iteration)
+                logger.add_images(f'Image/train_{model_name}', torch.stack([x.cpu(), x_hat.cpu()], dim=0).view(-1, *(x.shape[1:])), iteration)
             
                 
 
